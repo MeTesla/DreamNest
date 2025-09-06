2c5fb35265e31767df87048cfc43e52414affc3f
karim <pookarim@gmail.com>
2025-09-06 12:36:11 +0100

    ajout reservationModel du telephone

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,7 +2,7 @@ const express= require('express')
 const router= express.Router()
 
 router.get('/', (req, res)=>{
-    res.send('GET All from router')
+    res.send('<h1>GET All from router</h1>')
 })
 
 
