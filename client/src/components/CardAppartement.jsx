1ea5fe407ca12f89696a5270561a209179942705
karim Mohamed <pookarim@gmail.com>
2025-09-10 16:24:57 +0100

    Ajout de CardAppartement avec succès

--- client/src/components/CardAppartement.jsx
+++ client/src/components/CardAppartement.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 function CardAppartement({adresse, categorie, options, images}) {
   return (
-    <div>
-      <h3>{adresse}</h3>
-      <h3>{categorie}</h3>
-      <h3>{options}</h3>
-      <h3>{images}</h3>
+    <div style={{border: '1px solid'}}>
+      <h3>Adresse :</h3>
+      <div>{adresse}</div>
+      <h3>Catégorie</h3>
+      <div>{categorie}</div>
+      <h3>Options</h3>
+        <ul> {options[0].split(',').map((opt, ind)=>(
+                <li style={{listStyle:'square'}} key={ind}>{opt + " "}</li>
+            ))}
+        </ul> 
+      <h3>Images</h3>
+      <div className="images" style={{ display:'flex', gap:'30px'}}>
+            {images.map((img, key)=>(
+                <div key={key} style={{width:'200px', height:'140px'}}>
+                    <img src={`http://localhost:3000/${img.replace('public/','')}`} style={{width:'100%', height:'100%'}} />
+                </div>
+            ))}
+        </div>
+      <hr />
     </div>
   )
 }
